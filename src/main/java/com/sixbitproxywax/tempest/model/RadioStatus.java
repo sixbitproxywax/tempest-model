cfe1f642a135350f7332f0ea5a0f55e1a906fe46
Phillip Wright <github@phillipwright.info>
2024-10-20 22:57:47 -0500

    add logging and unknown radio status

--- src/main/java/com/sixbitproxywax/tempest/model/RadioStatus.java
+++ src/main/java/com/sixbitproxywax/tempest/model/RadioStatus.java
@@ -3,31 +3,40 @@ package com.sixbitproxywax.tempest.model;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public enum RadioStatus {
-    OFF(0),
-    ON(1),
-    ACTIVE(3),
-    BLE(7);
+  OFF(0),
+  ON(1),
+  ACTIVE(3),
+  BLE(7),
+  UNKNOWN(-1);
 
-    private static final Map<Integer, RadioStatus> lookup = new HashMap<>();
+  private static final Logger LOGGER = LoggerFactory.getLogger(RadioStatus.class);
+  private static final Map<Integer, RadioStatus> lookup = new HashMap<>();
 
-    static {
-        for (final var radioStatus : RadioStatus.values()) {
-            lookup.put(radioStatus.rawRepresentation(), radioStatus);
-        }
+  static {
+    for (final var radioStatus : RadioStatus.values()) {
+      lookup.put(radioStatus.rawRepresentation(), radioStatus);
     }
+  }
 
-    private final int rawRepresentation;
+  private final int rawRepresentation;
 
-    RadioStatus(final int rawRepresentation) {
-        this.rawRepresentation = rawRepresentation;
+  RadioStatus(final int rawRepresentation) {
+    this.rawRepresentation = rawRepresentation;
+  }
+
+  public static RadioStatus parse(int rawRepresentation) {
+    final var status = lookup.getOrDefault(rawRepresentation, UNKNOWN);
+    if (status == UNKNOWN) {
+      LOGGER.warn("Unknown Radio Status:" + rawRepresentation);
     }
+    return status;
+  }
 
-    public static RadioStatus parse(int rawRepresentation) {
-        return lookup.get(rawRepresentation);
-    }
-
-    public int rawRepresentation() {
-        return rawRepresentation;
-    }
+  public int rawRepresentation() {
+    return rawRepresentation;
+  }
 }
